bc94f18a344eeb1451e26981e62ae127f394cfe3
Sangkon, Han <sigmadream@gmail.com>
2024-09-12 23:49:48 +0900

    feat: GraphQL 기반 엔티티 추가 (#1)
    * Film 및 기초 엔티티 추가

---
+++ server/src/index.ts
@@ -0,0 +1,44 @@
+import "reflect-metadata";
+import express from "express";
+import http from "http";
+import cors from "cors";
+
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
+
+import { buildSchema } from "type-graphql";
+import { FilmResolver } from "./resolvers/Film";
+
+async function main() {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [FilmResolver],
+    }),
+    plugins: [ApolloServerPluginLandingPageLocalDefault()],
+  });
+  await apolloServer.start();
+  app.use(
+    "/graphql",
+    cors<cors.CorsRequest>(),
+    express.json(),
+    expressMiddleware(apolloServer)
+  );
+
+  httpServer.listen(process.env.PORT || 4000, () => {
+    if (process.env.NODE_ENV !== "production") {
+      console.log(`
+        server started on => http://localhost:4000
+        graphql playground => http://localhost:4000/graphql
+      `);
+    } else {
+      console.log(`
+        Production server Started...
+      `);
+    }
+  });
+}
+
+main().catch((err) => console.error(err));
