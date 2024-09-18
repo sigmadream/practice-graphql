10c215847c093441d06ffac455fec5dc573833a1
Sangkon Han <sigmadream@gmail.com>
2024-09-19 00:59:09 +0900

    // wip

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,23 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { Box, ChakraProvider, Text, theme } from "@chakra-ui/react";
 import * as React from "react";
+import FilmList from "./components/film/FilmList";
 
 const apolloClient = new ApolloClient({
   uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
 });
 
-export const App: React.FC = () => (
-  <ApolloProvider client={apolloClient}>
-    <ChakraProvider theme={theme}>
-      <Box>
-        <Text>Ghibli GraphQL</Text>
-      </Box>
-    </ChakraProvider>
-  </ApolloProvider>
-);
+export const App: React.FC = () => {
+  return (
+    <ApolloProvider client={apolloClient}>
+      <ChakraProvider theme={theme}>
+        <Box>
+          <Text>Ghibli GraphQL</Text>
+        </Box>
+
+        <FilmList />
+      </ChakraProvider>
+    </ApolloProvider>
+  );
+};
