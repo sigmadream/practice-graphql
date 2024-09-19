55b103d4925b398f1de9c7f03428c7213f11d525
Sangkon, Han <sigmadream@gmail.com>
2024-09-19 20:44:40 +0900

    feat: GraphQL 쿼리 (#2)

---
+++ web/src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ColorModeScript } from "@chakra-ui/react";
+import { App } from "./App";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ColorModeScript />
+    <App />
+  </StrictMode>
+);
